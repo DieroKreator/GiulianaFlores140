a1fb856e51ff4bdd25c020893b9e1b1f15a5c242
DieroKreator <qtechwired@gmail.com>
2024-03-24 21:42:48 -0300

    Added a negative test for login

--- test_login_negativo.py
+++ test_login_negativo.py
@@ -14,7 +14,7 @@ class TestLoginPositivo():
     def teardown_method(self, method):
         self.driver.quit()
 
-    @pytest.mark.skip(reason="temporaly ignore")
+    # @pytest.mark.skip(reason="temporaly ignore")
     def test_criacao_usuario(self):
         self.driver.get(self.url)
         self.driver.find_element(By.CSS_SELECTOR, "#perfil-hidden > img").click()
@@ -22,11 +22,7 @@ class TestLoginPositivo():
         self.driver.find_element(By.ID, "ContentSite_txtEmail").click()
         self.driver.find_element(By.ID, "ContentSite_txtEmail").send_keys("ruan_arthur_barros@dpi.indl.com.br")
         self.driver.find_element(By.ID, "ContentSite_txtPassword").click()
-        self.driver.find_element(By.ID, "ContentSite_txtPassword").send_keys("oBaa9f2Lgi")
+        self.driver.find_element(By.ID, "ContentSite_txtPassword").send_keys("wrongPassword")
         self.driver.find_element(By.CSS_SELECTOR, "#ContentSite_ibtContinue").click()
-        self.driver.find_element(By.CSS_SELECTOR, "#perfil-hidden > img").click()
 
-        assert self.driver.find_element(By.ID, "lblWelcome").text == "Boa Noite, Ruan!"
-
-        self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(10) > a:nth-child(2)").click()
-        self.driver.close()
+        assert self.driver.find_element(By.XPATH, "//span[contains(text(),'e-mail ou senha inválidos!')]").text == "e-mail ou senha inválidos!"
